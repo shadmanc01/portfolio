a99f12d796832f7903b8cd1dd037a126c363fe29
Shadman <shadmanchowdhury01@gmail.com>
2023-07-25 15:59:04 -0400

    style: headless UI

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17bf5e7c5ae85db1"; }
+/******/ 		__webpack_require__.h = function() { return "2d3261f207bb69ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
